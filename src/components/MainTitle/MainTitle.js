ef5a6cd35ceb6c82e803fd5dc268ffdf151b4da4
hyoonthedev <vincec1292@gmail.com>
2023-04-03 17:19:58 -0400

    Changed Landing Page Name

--- src/components/MainTitle/MainTitle.js
+++ src/components/MainTitle/MainTitle.js
@@ -4,7 +4,7 @@ import maplestoryLeaf from '../../assets/Images/maplestory-leaf.png';
 function MainTitle() {
     return(
         <header className="main-title">
-            <h3 className="main-title__text">Scroll Simulator</h3>
+            <h3 className="main-title__text">ScrollSim</h3>
             <img className="main-title__logo" src={maplestoryLeaf} alt="MapleStory leaf"/>
         </header>
     )
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -8,13 +8,13 @@ function LandingPage() {
 
     return(
         <section className="landing-page">
-            <h2 className="landing-page__title">Welcome to scrolls.im!</h2>
-            <p className="landing-page__text">scrolls.im is a Maplestory scrolling simulator designed for players that enjoy the thrill of scrolling.</p>
+            <h2 className="landing-page__title">Welcome to scrollsim!</h2>
+            <p className="landing-page__text">scrollsim is a Maplestory scrolling simulator designed for players that enjoy the thrill of scrolling.</p>
             <p className="landing-page__text">Choose between different modes!</p>
             <ul className="landing-page__list">
                 <li>Chaos Scroll</li>
-                <li>Regular, Dark and Burn Scroll</li>
-                <li>Aufheben Circlet</li>
+                <li>Regular, Dark and Burn Scroll <span className="landing-page__new">New!</span></li>
+                <li>Aufheben Circlet <span className="landing-page__new">New!</span></li>
             </ul>
             <p className="landing-page__text">Try this simulator before you <span className="landing-page__red-text">risk it all</span> on your items!</p>
             <p className="landing-page__text">Enjoy and have fun!</p>
